534622aeacb8fc57aa79b4c21f2a3b23ac526675
lautaro <lautaguzman21@hotmail.com>
2023-06-27 15:30:50 -0300

    AGREGANDO JSON

--- page/carrito.js
+++ page/carrito.js
@@ -3,22 +3,32 @@ const verCarrito = document.querySelector("#Vercarrito");
 const modalContainer = document.querySelector("#modal-container");
 
 verCarrito.addEventListener("click", () => {
-
-
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
     modalHeader.innerHTML = `
       <h1>resumen del pedido</h1>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 `;
     modalContainer.append(modalHeader);
+
+    const buttonClose = document.createElement("button")
+    buttonClose.innerHTML = `cerrar`
+
+    buttonClose.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+    modalHeader.append(buttonClose)
 
     carrito.forEach((s) => {
         let modalMain = document.createElement("div");
         modalMain.className = "modal-main";
         modalMain.innerHTML = `
         <img src="${s.img}">
+        <button>➖</button>
         <h2>${s.nombre}</h2>
+        <button>➕</button>
+        <button>❌</button>
         <p>$${s.precio}</p>
        `;
         modalContainer.append(modalMain);
