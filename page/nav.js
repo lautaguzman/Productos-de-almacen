7b41386ed0664ea4e74c63260a90cc6258af8205
Lautaro Guzman <lautaguzman21@hotmail.com>
2024-06-01 19:04:56 -0300

    responsive mobile

--- page/nav.js
+++ page/nav.js
@@ -4,21 +4,6 @@ const inicio = document.querySelector("#inicio");
 // Agregamos un evento de clic al botón "Inicio" para recargar la página
 inicio.addEventListener("click", recargarPag);
 
-// Capturamos los elementos para abrir y cerrar la barra de navegación en dispositivos móviles
-const openItems = document.querySelector("#openItems");
-const closeItems = document.querySelector("#closeItems");
-const navItems = document.querySelector("#navItems");
-
-// Agregamos un evento de clic al botón para abrir la barra de navegación
-openItems.addEventListener("click", () => {
-    navItems.style.display = "flex";
-});
-
-// Agregamos un evento de clic al botón para cerrar la barra de navegación
-closeItems.addEventListener("click", () => {
-    navItems.style.display = "none";
-});
-
 // Función para recargar la página
 function recargarPag() {
     location.reload(); // Recarga la página actual
